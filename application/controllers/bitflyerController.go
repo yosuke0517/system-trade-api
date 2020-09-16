55109cfd85847ba5eb1402cc75e1152ede4771ce
takeuchiyosuke <x.clover.x007@gmail.com>
2020-09-16 18:42:54 +0900

    fix: 開始条件変更

--- application/controllers/bitflyerController.go
+++ application/controllers/bitflyerController.go
@@ -107,7 +107,7 @@ SystemTrade:
 				}
 			}
 			// 0秒台で分析・システムトレードを走らせる
-			if (time.Now().Truncate(time.Second).Minute()%10 == 0 || time.Now().Truncate(time.Second).Minute() == 0) && time.Now().Truncate(time.Second).Second() == 0 {
+			if (time.Now().Truncate(time.Second).Minute()%10 == 0 || time.Now().Truncate(time.Second).Minute() == 0) && (time.Now().Truncate(time.Second).Second() == 0 || time.Now().Truncate(time.Second).Second() == 50) {
 				currentCollateral, err := bitflyerClient.GetCollateral()
 				closeOrderExecutionCheck = service.CloseOrderExecutionCheck()
 				if err != nil {
@@ -216,7 +216,7 @@ SystemTrade:
 			}
 
 			// 注文準備
-			if (time.Now().Truncate(time.Second).Minute()%10 == 9 || time.Now().Truncate(time.Second).Minute() == 9) && (time.Now().Truncate(time.Second).Second() == 28 || time.Now().Truncate(time.Second).Second() == 58) {
+			if (time.Now().Truncate(time.Second).Minute()%10 == 9 || time.Now().Truncate(time.Second).Minute() == 9) && time.Now().Truncate(time.Second).Second() == 58 {
 				params := map[string]string{
 					"product_code":      "FX_BTC_JPY",
 					"child_order_state": "ACTIVE",
