dc377684ea1b8855fcdaa5ab9b493305195856f7
yosuke0517 <x.clover.x007@gmail.com>
2020-11-01 17:45:10 +0900

    feat: メモリリーク調査用ログ

--- domain/service/create_candle_with_duration.go
+++ domain/service/create_candle_with_duration.go
@@ -10,23 +10,23 @@ import (
 // キャンドル情報を保存する
 func CreateCandleWithDuration(ticker bitflyer.Ticker, productCode string, duration time.Duration) bool {
 	currentCandle := candle.SelectOne(productCode, duration, ticker.TruncateDateTime(duration))
-	price := ticker.GetMidPrice()
+	// price := ticker.GetMidPrice()
 	// 秒単位は毎回insert
 	if currentCandle == nil {
-		candle := candle.NewCandle(productCode, duration, ticker.TruncateDateTime(duration),
-			price, price, price, price, ticker.Volume)
-		candle.Insert()
+		//candle := candle.NewCandle(productCode, duration, ticker.TruncateDateTime(duration),
+		//	price, price, price, price, ticker.Volume)
+		//candle.Insert()
 		return true
 	}
 	// 分・時単位は秒単位ではupdateする
-	if currentCandle.High <= price {
-		currentCandle.High = price
-	} else if currentCandle.Low >= price {
-		currentCandle.Low = price
-	}
-	currentCandle.Volume += ticker.Volume
-	currentCandle.Close = price
-	currentCandle.Save()
+	//if currentCandle.High <= price {
+	//	currentCandle.High = price
+	//} else if currentCandle.Low >= price {
+	//	currentCandle.Low = price
+	//}
+	//currentCandle.Volume += ticker.Volume
+	//currentCandle.Close = price
+	//currentCandle.Save()
 	return false
 }
 
